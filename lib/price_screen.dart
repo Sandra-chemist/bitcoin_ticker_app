494f3acab9061aa73ce1b682c2cea020b5141d0a
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-27 11:09:59 +0200

    code was refactored - ternary operator was added

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -53,15 +53,6 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  Widget getPicker() {
-    if (Platform.isIOS) {
-      return iOSPicker();
-    } else if (Platform.isAndroid) {
-      return androidDropdown();
-    }
-    return getPicker();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,7 +89,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: getPicker(),
+            child: Platform.isAndroid ? androidDropdown() : iOSPicker(),
           ),
         ],
       ),
